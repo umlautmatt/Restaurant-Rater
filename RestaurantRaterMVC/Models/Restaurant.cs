d68ebd2d2c330bb6abad1c760b5da41b59cc07cf
umlautmatt <mahawley24@gmail.com>
2019-04-29 10:42:16 -0400

    main solution saved

--- RestaurantRaterMVC/Models/Restaurant.cs
+++ RestaurantRaterMVC/Models/Restaurant.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,17 @@ namespace RestaurantRaterMVC.Models
 {
     public class Restaurant
     {
+        public int RestaurantID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Rating { get; set; }
+
+
     }
+
+    public class RestaurantDBContext : DbContext //DbContext is a snapshot of database
+    {
+        public DbSet<Restaurant> Restaurants { get; set; } //DbSet creates the table for the database
+    }
+
 }
