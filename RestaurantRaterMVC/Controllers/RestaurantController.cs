f5ff34e02aee039a3762ecdaa5326b74fef5c6f4
umlautmatt <mahawley24@gmail.com>
2019-04-29 16:33:52 -0400

    get details method

--- RestaurantRaterMVC/Controllers/RestaurantController.cs
+++ RestaurantRaterMVC/Controllers/RestaurantController.cs
@@ -40,6 +40,22 @@ namespace RestaurantRaterMVC.Controllers
             return View(restaurant);
         }
 
+        // GET: Restaurant/Details
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+
+
         // GET: Restuarant/Edit
         public ActionResult Edit(int? id)
         {
