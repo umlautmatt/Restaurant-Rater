62f3c97d74352c970ef697fae548774ea9d78010
umlautmatt <mahawley24@gmail.com>
2019-04-29 14:59:08 -0400

    added delete functionality

--- RestaurantRaterMVC/Controllers/RestaurantController.cs
+++ RestaurantRaterMVC/Controllers/RestaurantController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -37,5 +38,22 @@ namespace RestaurantRaterMVC.Controllers
             }
             return View(restaurant);
         }
+
+        // GET: Restaurant/Delete
+        public ActionResult Delete(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+
+        // POST: Restaurant/Delete
     }
 }
