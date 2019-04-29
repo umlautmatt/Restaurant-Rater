c633a7c7b46b1f0ab3997359ae0d8c1d3d81cb8d
umlautmatt <mahawley24@gmail.com>
2019-04-29 11:59:48 -0400

    updated restaurant controller, model, and index

--- RestaurantRaterMVC/Controllers/RestaurantController.cs
+++ RestaurantRaterMVC/Controllers/RestaurantController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestaurantRaterMVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,13 @@ namespace RestaurantRaterMVC.Controllers
 {
     public class RestaurantController : Controller
     {
+        private RestaurantDBContext _db = new RestaurantDBContext();
+
+
         // GET: Restaurant
         public ActionResult Index()
         {
-            return View();
+            return View(_db.Restaurants.ToList());
         }
     }
 }
